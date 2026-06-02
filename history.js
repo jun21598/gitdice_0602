b2c25d38a6ab93bb956cd63d917eb8ddb2ed3cb2
jun21598 <vfa01-wi260012@sankogakuen.jp>
2026-06-02 14:59:30 +0900

    8回の２

--- history.js
+++ history.js
@@ -2,9 +2,10 @@
 const gameHistory = [
     // ここから下に追記していく
 { turn: 1, player: "playerA", finalPos: "三崎口", event: "無事に移動した！", foundTreasure: false },
-];{ turn: 1, player: "playerA", finalPos: "秦野", event: "無事に移動した！", foundTreasure: false },
+{ turn: 1, player: "playerA", finalPos: "秦野", event: "無事に移動した！", foundTreasure: false },
 { turn: 1, player: "playerA", finalPos: "箱根湯本", event: "無事に移動した！", foundTreasure: false },
 { turn: 1, player: "playerA", finalPos: "小田原", event: "無事に移動した！", foundTreasure: false },
 { turn: 1, player: "playerA", finalPos: "熱海", event: "無事に移動した！", foundTreasure: false },
 { turn: 1, player: "playerA", finalPos: "伊東", event: "無事に移動した！", foundTreasure: false },      
 { turn: 1, player: "playerA", finalPos: "湯河原", event: "無事に移動した！", foundTreasure: false },
+];
