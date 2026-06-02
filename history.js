dd54fbfde9bfad18117a7046b8c6d922f71288aa
jun21598 <vfa01-wi260012@sankogakuen.jp>
2026-06-02 14:34:25 +0900

    8回目の結果

--- history.js
+++ history.js
@@ -1,5 +1,10 @@
 // ⚠️ 各ターンの終わりに、画面に表示された自分の結果コードをこの配列の中に追加してください。
 const gameHistory = [
     // ここから下に追記していく
-
-];
+{ turn: 1, player: "playerA", finalPos: "三崎口", event: "無事に移動した！", foundTreasure: false },
+];{ turn: 1, player: "playerA", finalPos: "秦野", event: "無事に移動した！", foundTreasure: false },
+{ turn: 1, player: "playerA", finalPos: "箱根湯本", event: "無事に移動した！", foundTreasure: false },
+{ turn: 1, player: "playerA", finalPos: "小田原", event: "無事に移動した！", foundTreasure: false },
+{ turn: 1, player: "playerA", finalPos: "熱海", event: "無事に移動した！", foundTreasure: false },
+{ turn: 1, player: "playerA", finalPos: "伊東", event: "無事に移動した！", foundTreasure: false },      
+{ turn: 1, player: "playerA", finalPos: "湯河原", event: "無事に移動した！", foundTreasure: false },
